096676202f8114bd39b4703c6061e056f0807320
ilaypilo <ilay.pilo@gmail.com>
2019-08-13 22:52:25 +0300

    Added MVVM to Login screen

--- app/src/main/java/com/colman/finalproject/bases/GagBaseViewModel.java
+++ app/src/main/java/com/colman/finalproject/bases/GagBaseViewModel.java
@@ -4,13 +4,22 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.colman.finalproject.model.Model;
+import com.colman.finalproject.utils.SingleLiveEvent;
 
 public class GagBaseViewModel extends AndroidViewModel {
     protected Model mModel = Model.getInstance();
+    protected MutableLiveData<Boolean> mIsLoggedIn = new SingleLiveEvent<>();
 
     public GagBaseViewModel(@NonNull Application application) {
         super(application);
+    }
+
+    public void observeLoggedInLiveData(LifecycleOwner lifecycleOwner, Observer<Boolean> observer) {
+        mIsLoggedIn.observe(lifecycleOwner, observer);
     }
 }
